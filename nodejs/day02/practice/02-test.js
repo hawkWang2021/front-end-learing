d8f05ddb1b890a5c6c3347a0d4a56cff781726c4
HawkWang <wph2021@foxmail.com>
2022-06-23 09:00:37 +0800

    update day02

--- nodejs/day02/practice/02-test.js
+++ nodejs/day02/practice/02-test.js
@@ -1,7 +1,7 @@
 // #模块作用域
-const custom = require('./01-custom');
+const custom = require('./01-custom')
 // 在 02-test.js 模块中,无法访问到 01-custom.js 模块中的私有成员
-console.log('custom: ', custom); // {}
+console.log('custom: ', custom)
 
 // module 对象
-console.log(module);
+console.log(module)
--- nodejs/day02/practice/04-test.js
+++ nodejs/day02/practice/04-test.js
@@ -1,5 +1,5 @@
 // #向外共享模块作用域的成员;
 // MARK:使用 require() 方法导入模块时,导入的结果,永远以 module.exports 指向的对象为准
 // 1.导入模块 m1
-const m1 = require('./03-m1');
-console.log(m1); // { nickname: '小黑', sayHi: [Function: sayHi] }
+const m1 = require('./03-m1')
+console.log(m1) // { nickname: '小黑', sayHi: [Function: sayHi] }
