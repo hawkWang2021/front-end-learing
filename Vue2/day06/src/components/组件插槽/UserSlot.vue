b8cd6f195956852a47f586fa3767763bb883c91b
HawkWang <wph2021@foxmail.com>
2022-06-27 10:13:38 +0800

    update package.json

--- Vue2/day06/src/components/组件插槽/UserSlot.vue
+++ Vue2/day06/src/components/组件插槽/UserSlot.vue
@@ -35,6 +35,17 @@
           <p>一片冰心在玉壶。</p>
         </template>
       </panel-comp>
+      <panel-comp>
+        <template #title>
+          <h4 style="color: purple">芙蓉楼送辛渐</h4>
+        </template>
+        <template slot="content">
+          <p>寒雨连江夜入吴,</p>
+          <p>平明送客楚山孤。</p>
+          <p>洛阳亲友如相问，</p>
+          <p>一片冰心在玉壶。</p>
+        </template>
+      </panel-comp>
     </div>
   </div>
 </template>
